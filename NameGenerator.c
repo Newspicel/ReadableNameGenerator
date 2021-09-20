e6648b6c8037f5903ec4b4678cfd03134669fb86
Restyled.io <commits@restyled.io>
2021-09-20 17:42:37 +0000

    Restyled by clang-format

--- NameGenerator.c
+++ NameGenerator.c
@@ -6,31 +6,29 @@
 
 #include <time.h>
 
-typedef struct
-{
-    char *letter;
-    char *chars[16];
+typedef struct {
+  char *letter;
+  char *chars[16];
 } speakableChars;
 
-speakableChars *linearSearch(speakableChars *items, size_t size, const char *letter)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        if (strcmp(&items[i].letter, &letter) == 0)
-        {
-            return &items[i];
-        }
+speakableChars *linearSearch(speakableChars *items, size_t size,
+                             const char *letter) {
+  for (size_t i = 0; i < size; i++) {
+    if (strcmp(&items[i].letter, &letter) == 0) {
+      return &items[i];
     }
+  }
 }
 
-int randomNumber(int min_num, int max_num)
-{
-    int result = (rand() % (max_num + 1 - min_num)) + min_num;
-    return result;
+int randomNumber(int min_num, int max_num) {
+  int result = (rand() % (max_num + 1 - min_num)) + min_num;
+  return result;
 }
 
 speakableChars speakable[] = {
-    {'a', {'b', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'u'}},
+    {'a',
+     {'b', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't',
+      'u'}},
     {'b', {'a', 'e', 'i', 'l', 'o', 'r', 'u'}},
     {'c', {'h'}},
     {'d', {'a', 'e', 'i', 'l', 'o', 'r', 'u'}},
@@ -44,7 +42,9 @@ speakableChars speakable[] = {
     {'l', {'a', 'e', 'i', 'o', 'u'}},
     {'m', {'a', 'e', 'i', 'l', 'o', 'u'}},
     {'n', {'a', 'e', 'i', 'o', 'u'}},
-    {'o', {'b', 'c', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't'}},
+    {'o',
+     {'b', 'c', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's',
+      't'}},
     {'p', {'a', 'e', 'i', 'l', 'o', 'r', 'u'}},
     {'r', {'a', 'e', 'i', 'o', 'u'}},
     {'s', {'a', 'e', 'c', 'i', 'o', 'u', 't'}},
@@ -53,32 +53,29 @@ speakableChars speakable[] = {
 };
 
 char starts[19] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k',
-                   'l', 'm', 'n', 'o', 'r', 'p', 's', 't', 'u'
-                  };
+                   'l', 'm', 'n', 'o', 'r', 'p', 's', 't', 'u'};
 
-char *generate()
-{
-    srand(time(NULL));
-    char *name = (char *)malloc(128 * sizeof(char));
+char *generate() {
+  srand(time(NULL));
+  char *name = (char *)malloc(128 * sizeof(char));
 
-    int minLength = 5;
-    int maxLength = 10;
+  int minLength = 5;
+  int maxLength = 10;
 
-    int length = randomNumber(4, 9);
-    int startingCharIndex = randomNumber(0, 18);
+  int length = randomNumber(4, 9);
+  int startingCharIndex = randomNumber(0, 18);
 
-    name[0] = starts[startingCharIndex];
+  name[0] = starts[startingCharIndex];
 
-    size_t size = sizeof(speakable) / sizeof(speakableChars);
-    speakableChars *found;
+  size_t size = sizeof(speakable) / sizeof(speakableChars);
+  speakableChars *found;
 
-    for (int i = 0; i < length; i++)
-    {
-        found = linearSearch(speakable, size, name[i]);
-        name[i + 1] = found->chars[randomNumber(0, strlen(found->chars))];
-    }
+  for (int i = 0; i < length; i++) {
+    found = linearSearch(speakable, size, name[i]);
+    name[i + 1] = found->chars[randomNumber(0, strlen(found->chars))];
+  }
 
-    name[length + 1] = '\0';
+  name[length + 1] = '\0';
 
-    return name;
+  return name;
 }
