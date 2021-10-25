2046f94d184f2e6ce842e3a0a6ded821ba199b17
Julian <newspicel11@gmail.com>
2021-10-25 11:43:48 +0200

    Create NameGenerator.go
    Co-Author: OPEN-AI davinci-codex

---
+++ NameGenerator.go
@@ -0,0 +1,44 @@
+import (
+    "math/rand"
+)
+
+var (
+    starts = []string{"a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "l", "m", "n", "o", "r", "p", "s", "t", "u"}
+    speakable = map[string][]string{
+        "a": {"b", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "r", "s", "t", "u"},
+        "b": {"a", "e", "i", "l", "o", "r", "u"},
+        "c": {"h"},
+        "d": {"a", "e", "i", "l", "o", "r", "u"},
+        "e": {"f", "i", "j", "k", "m", "n", "p", "r", "s", "t", "u"},
+        "f": {"a", "e", "i", "l", "o", "r", "u"},
+        "g": {"a", "e", "i", "l", "o", "u"},
+        "h": {"a", "e", "i", "o", "u"},
+        "i": {"b", "f", "k", "l", "m", "n", "p", "r", "h", "s", "t"},
+        "j": {"a", "e", "i", "o", "u"},
+        "k": {"a", "e", "i", "l", "o", "u"},
+        "l": {"a", "e", "i", "o", "u"},
+        "m": {"a", "e", "i", "l", "o", "u"},
+        "n": {"a", "e", "i", "o", "u"},
+        "o": {"b", "c", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "r", "s", "t"},
+        "p": {"a", "e", "i", "l", "o", "r", "u"},
+        "r": {"a", "e", "i", "o", "u"},
+        "s": {"a", "e", "c", "i", "o", "u", "t"},
+        "t": {"a", "e", "i", "o", "u"},
+        "u": {"b", "g", "i", "k", "n", "m", "p", "r", "s", "t"},
+    }
+    minLength = 5
+    maxLength = 10
+)
+
+func Generate() string {
+    length := rand.Intn(maxLength-minLength) + minLength
+    char := starts[rand.Intn(len(starts))]
+
+    name := char
+
+    for i := 0; i < length; i++ {
+        char = speakable[char][rand.Intn(len(speakable[char]))]
+        name += char
+    }
+    return name
+}
